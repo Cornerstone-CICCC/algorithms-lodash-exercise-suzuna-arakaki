170724d20e024b0f1a169df9d8364f42c5a90a97
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-10-02 20:39:03 -0700

    ex01 is completed

--- ex01.js
+++ ex01.js
@@ -4,8 +4,6 @@ const _ = require('lodash');
 
 const numbers = [1, 2, 3, 4, 5];
 
-const doubled = _(numbers)
-.map((num) => num * 2)
-.value()
+const doubled = _.map(numbers, (num) => num * 2)
 
 console.log(doubled); // Expected output: [2, 4, 6, 8, 10]
