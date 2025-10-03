36b1f0662bdc46fd184efa7d35c36f6f4fab8ec0
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-10-02 20:21:14 -0700

    ex07 is completed

--- ex07.js
+++ ex07.js
@@ -1,4 +1,5 @@
-// Instructions: Use lodash's orderBy function to sort the array of objects by the "age" property in descending order.
+// Instructions: Use lodash's orderBy function to sort the array of objects 
+// by the "age" property in descending order.
 
 const _ = require('lodash');
 
@@ -8,6 +9,7 @@ const people = [
   { name: 'Jim', age: 20 }
 ];
 
-// your code here
+const sortedPeople = _.orderBy(people, ["age"], ["desc"])
 
-console.log(sortedPeople); // Expected output: [{ name: 'Jane', age: 30 }, { name: 'John', age: 25 }, { name: 'Jim', age: 20 }]
+console.log(sortedPeople); 
+// Expected output: [{ name: 'Jane', age: 30 }, { name: 'John', age: 25 }, { name: 'Jim', age: 20 }]
